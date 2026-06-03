69f944040e4fec040f89cf458a7be1e0c48fa2bb
fabian1318 <fabian1318@gmail.com>
2026-06-03 03:20:18 -0400

    Implementar el diseño y los estilos de la página principal para la biblioteca de manga.

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Manga
 
 def inicio(request):
-    # Por ahora solo renderizará un archivo HTML simple
-    return render(request, 'biblioteca/index.html')
+    # Traemos todos los mangas, ordenados por los más recientes primero
+    lista_mangas = Manga.objects.all().order_by('-fecha_agregado')
+    
+    # Empaquetamos los datos en un "diccionario de contexto" para enviarlos al HTML
+    contexto = {
+        'mangas': lista_mangas
+    }
+    
+    return render(request, 'biblioteca/index.html', contexto)
